89f06f89fe7bb02ae3bb90ff7f46e422fcd16cea
John Riselvato <jdriselvato@gmail.com>
2016-04-08 16:05:54 -0400

    refactored multi-simple with better practices and more dynamic sqaure generation

--- examples/multi-simple/source/template.c
+++ examples/multi-simple/source/template.c
@@ -2,7 +2,7 @@
 Generating a multiple sprites
 
 This project demos the ability to dynamically add sprites to the screen.
-Pressing the key up adds another sprite to the screen
+Pressing the key A adds another sprite to the screen
 -- John Riselvato ( March 26th, 2016 )
 
 What I listened to while developing:
@@ -16,65 +16,55 @@ so much, it was my buddy growing up. Thanks Nintendo.
 built with: Nintendo DS rom tool 1.50.3 - Dec 12 2015
 
 Things to know:
-- OAM is the Object Attributed Memory, it's what manages all the sprites.
+- OAM is the Object Attributed Memory, it's what manages all the sprites in memory.
 - BG_GFX_SUB comes from video.h and is the brief background graphics memory (sub engine)
-- SCREEN_BASE_BLOCK_SUB comes from background.h and
-	is a macro which returns a u16* pointer to background Map ram (Sub Engine)
+- oamAllocateGfx should be called only once per sprite
 ---------------------------------------------------------------------------------*/
-
 #include <nds.h>
 #include <stdio.h>
 
 typedef struct {
 	u16* gfx;
-	SpriteSize size;
-	SpriteColorFormat format;
 	int color;
-	int rotationIndex;
-	int paletteAlpha;
 	int x, y;
 } Sprite;
 
-Sprite sprites[100] = { // maximum amount of sprites allocated
-	{0, SpriteSize_16x16, SpriteColorFormat_Bmp, ARGB16(1, 31, 12, 12), 0, 15, 20, 15},
-	{0, SpriteSize_16x16, SpriteColorFormat_Bmp, ARGB16(1, 12, 31, 12), 0, 15, 20, 80},
+Sprite sprites[100] = { // maximum amount of sprites allocated 100
+	{0, ARGB16(1, 31, 12, 12), 20, 15},
 };
+int sprite_count = 1; // keep track of how many sprites on are the screen so we can append properly
 
-void createSquare(Sprite sprite, int count);
+void createSquare(Sprite tmp_sprite, int count); // sets up oam details
+void addNewSquare(); // adds square to memory both gfx and Sprites array
+
 int main(int argc, char** argv) {
+	// constants
+	int angle = 0;
+
 	// Initialize bottom screen
 	videoSetModeSub(MODE_0_2D);
 	vramSetBankD(VRAM_D_SUB_SPRITE);
 	consoleDemoInit(); // this has to be before oamInit else text and sprites don't show on screen
 	oamInit(&oamSub, SpriteMapping_Bmp_1D_128, false);
 
-	int angle = 0;
-	int sprite_count = 2; // keep track of how many sprites on are the screen so we can append properly
+	sprites[0].gfx = oamAllocateGfx(&oamSub, SpriteSize_16x16, SpriteColorFormat_Bmp);
 
 	while(1) {
 		iprintf("\x1b[1;1HNumber of sprites: %d", sprite_count);
 
+		// display sprites
 		for(int i = 0; i < sprite_count; i++) {
 			createSquare(sprites[i], i);
 		}
 
-		// oamRotateScale(&oamSub, 0, angle, (1 << 8), (1 << 8));
-		// angle += 45;
+		// react to key A and add new square
+		scanKeys();
+		if(keysHeld() & KEY_A && sprite_count < 100)
+			addNewSquare();
 
-	 //   	scanKeys();
-		// int keys = keysDown();
-
-		// if(keys & KEY_UP) { // add a new square when up key is pressed
-		// 	if (sprite_count < 100) {
-		// 		int color = ARGB16(1, rand() % 31 + 1, rand() % 31 + 1, rand() % 31 + 1);
-
-		// 		int rand_x = rand() % SCREEN_WIDTH + 1; // get random x location to place square on
-		// 		int rand_y = rand() % SCREEN_HEIGHT + 1; // get random y location to place square on
-		// 		Sprite tmp = {0, SpriteSize_16x16, SpriteColorFormat_Bmp, color, 0, 15, rand_x, rand_y}; // create a sprite
-		// 		sprites[sprite_count + 1] = tmp; // add sprite to our sprites array
-		// 		sprite_count++; // increase sprite_count one
-		// 	}
-		// }
+		// rotate squares
+		oamRotateScale(&oamSub, 0, angle, (1 << 8), (1 << 8));
+		angle += 90;
 
 		swiWaitForVBlank(); // draw updates
 		oamUpdate(&oamSub); //send the updates to the hardware
@@ -82,24 +72,33 @@ int main(int argc, char** argv) {
 	return 0;
 }
 
+// adds new square to memory
+void addNewSquare() {
+	int color = ARGB16(1, rand() % 31 + 1, rand() % 31 + 1, rand() % 31 + 1);
+	int rand_x = rand() % SCREEN_WIDTH + 1; // get random x location to place square on
+	int rand_y = rand() % SCREEN_HEIGHT + 1; // get random y location to place square on
+
+	Sprite tmp = {0, color, rand_x, rand_y}; // create a sprite
+	tmp.gfx = oamAllocateGfx(&oamSub, SpriteSize_16x16, SpriteColorFormat_Bmp);
+
+	sprites[sprite_count + 1] = tmp; // add sprite to our sprites array
+	sprite_count++; // increase sprite_count one
+}
+
 // createSquare is a function that easily allows us to add a sprite on the screen with various properties.
-void createSquare(Sprite sprite, int count) {
-	if (sprite.gfx == 0)	// we only need to allocate space the first time the sprite is created
-		sprite.gfx = oamAllocateGfx(&oamSub, SpriteSize_16x16, SpriteColorFormat_Bmp);
+void createSquare(Sprite tmp_sprite, int count) {
+	dmaFillHalfWords(tmp_sprite.color, tmp_sprite.gfx, 16*16*2); // fill each square with correct color
 
-	if (sprite.color == ARGB16(1, 12, 31, 12)) sprite.x = 100;
-
-	dmaFillHalfWords(sprite.color, sprite.gfx, 16*16*2); // fill each square with correct color
 	oamSet(
 		&oamSub, // sub display
 		count, // oam entry to set
-		sprite.x, sprite.y, //position
+		tmp_sprite.x, tmp_sprite.y, //position
 		0, // priority
 		15, // palette for 16 color sprite or alpha for bmp sprite
 		SpriteSize_16x16,
 		SpriteColorFormat_Bmp,
-		sprite.gfx,
-		sprite.rotationIndex,
+		tmp_sprite.gfx,
+		0, // rotation
 		true, // double the size of rotated sprites
 		false, // don't hide the sprite
 		false, false, // vflip, hflip
