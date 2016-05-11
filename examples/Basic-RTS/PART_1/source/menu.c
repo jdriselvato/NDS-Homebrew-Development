ab3ac5563a33d0adfd8e3a51ef245010eb892cd6
John Riselvato <jdriselvato@gmail.com>
2016-05-11 15:27:03 -0400

    got rid of warnings but not seeing anything now...

--- examples/Basic-RTS/PART_1/source/menu.c
+++ examples/Basic-RTS/PART_1/source/menu.c
@@ -5,14 +5,12 @@ extern const unsigned int spritesheetTiles[2880];
 
 Menu initMenu() {
 	Menu menu = {SCREEN_WIDTH / 2 - 16, SCREEN_HEIGHT - 16};
-	u16* gfx_array[5] = {
-		oamAllocateGfx(&oamSub, SpriteSize_16x16, SpriteColorFormat_256Color),
-		oamAllocateGfx(&oamSub, SpriteSize_16x16, SpriteColorFormat_256Color),
-		oamAllocateGfx(&oamSub, SpriteSize_16x16, SpriteColorFormat_256Color),
-		oamAllocateGfx(&oamSub, SpriteSize_16x16, SpriteColorFormat_256Color),
-		oamAllocateGfx(&oamSub, SpriteSize_16x16, SpriteColorFormat_256Color),
-		};
-	menu.gfx = gfx_array;
+	menu.gfx[0] = oamAllocateGfx(&oamSub, SpriteSize_16x16, SpriteColorFormat_256Color);
+	menu.gfx[1] = oamAllocateGfx(&oamSub, SpriteSize_16x16, SpriteColorFormat_256Color);
+	menu.gfx[2] = oamAllocateGfx(&oamSub, SpriteSize_16x16, SpriteColorFormat_256Color);
+	menu.gfx[3] = oamAllocateGfx(&oamSub, SpriteSize_16x16, SpriteColorFormat_256Color);
+	menu.gfx[4] = oamAllocateGfx(&oamSub, SpriteSize_16x16, SpriteColorFormat_256Color);
+
 	menu.gfx_frame = (u8*)spritesheetTiles;
 
 	return menu;
@@ -20,7 +18,7 @@ Menu initMenu() {
 
 void displayMenu(Menu * menu, bool hideMenu) {
 	for (int i = 0; i < 5; i++) {
-		u8* offset = menu->gfx_frame + (5 + i) * 16*16;
+		u8* offset = menu->gfx_frame + (5) * 16*16;
 		dmaCopy(offset, menu->gfx, 16*16);
 
 		oamSet(&oamSub,
--- examples/Basic-RTS/PART_1/source/menu.h
+++ examples/Basic-RTS/PART_1/source/menu.h
@@ -1,6 +1,6 @@
 typedef struct {
 	int x, y; // location on screen
-	u16* gfx;
+	u16* gfx[5];
 	u8* gfx_frame;
 	bool shouldDisplay;
 } Menu;
