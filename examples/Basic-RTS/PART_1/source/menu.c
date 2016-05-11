b98ff169892757385e53fd31140eeb1c25b815c8
John Riselvato <jdriselvato@gmail.com>
2016-05-11 15:18:55 -0400

    Im having issues with arrays but getting close

--- examples/Basic-RTS/PART_1/source/menu.c
+++ examples/Basic-RTS/PART_1/source/menu.c
@@ -5,24 +5,33 @@ extern const unsigned int spritesheetTiles[2880];
 
 Menu initMenu() {
 	Menu menu = {SCREEN_WIDTH / 2 - 16, SCREEN_HEIGHT - 16};
-	menu.gfx = oamAllocateGfx(&oamSub, SpriteSize_16x16, SpriteColorFormat_256Color);
+	u16* gfx_array[5] = {
+		oamAllocateGfx(&oamSub, SpriteSize_16x16, SpriteColorFormat_256Color),
+		oamAllocateGfx(&oamSub, SpriteSize_16x16, SpriteColorFormat_256Color),
+		oamAllocateGfx(&oamSub, SpriteSize_16x16, SpriteColorFormat_256Color),
+		oamAllocateGfx(&oamSub, SpriteSize_16x16, SpriteColorFormat_256Color),
+		oamAllocateGfx(&oamSub, SpriteSize_16x16, SpriteColorFormat_256Color),
+		};
+	menu.gfx = gfx_array;
 	menu.gfx_frame = (u8*)spritesheetTiles;
 
 	return menu;
 }
 
 void displayMenu(Menu * menu, bool hideMenu) {
-	u8* offset = menu->gfx_frame + 5 * 16*16;
-	dmaCopy(offset, menu->gfx, 16*16);
+	for (int i = 0; i < 5; i++) {
+		u8* offset = menu->gfx_frame + (5 + i) * 16*16;
+		dmaCopy(offset, menu->gfx, 16*16);
 
-	oamSet(&oamSub,
-		2, // oam entry id
-		menu->x, menu->y, // x, y location
-		0, 15, // priority, palette
-		SpriteSize_16x16,
-		SpriteColorFormat_256Color,
-		menu->gfx, // the oam gfx
-		-1, false,
-		hideMenu, // hide me?
-		false, false, false);
+		oamSet(&oamSub,
+			2 + i, // oam entry id
+			menu->x + (16 * i), menu->y, // x, y location
+			0, 15, // priority, palette
+			SpriteSize_16x16,
+			SpriteColorFormat_256Color,
+			menu->gfx[i], // the oam gfx
+			-1, false,
+			hideMenu, // hide me?
+			false, false, false);
+	}
 }
--- examples/Basic-RTS/PART_1/source/menu.h
+++ examples/Basic-RTS/PART_1/source/menu.h
@@ -1,7 +1,7 @@
 typedef struct {
 	int x, y; // location on screen
-	u16* gfx[5];
-	u8* gfx_frame[5];
+	u16* gfx;
+	u8* gfx_frame;
 	bool shouldDisplay;
 } Menu;
 
