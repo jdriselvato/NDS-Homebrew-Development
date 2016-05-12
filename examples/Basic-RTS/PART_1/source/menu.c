8c8d3ee87533077808314e58d30f90e8c420d6dd
John Riselvato <jdriselvato@gmail.com>
2016-05-12 12:00:15 -0400

    We have a menu showing! Thanks Matt for letting me talk it out to you.

--- examples/Basic-RTS/PART_1/source/menu.c
+++ examples/Basic-RTS/PART_1/source/menu.c
@@ -5,21 +5,18 @@ extern const unsigned int spritesheetTiles[2880];
 
 Menu initMenu() {
 	Menu menu = {SCREEN_WIDTH / 2 - 16, SCREEN_HEIGHT - 16};
-	menu.gfx[0] = oamAllocateGfx(&oamSub, SpriteSize_16x16, SpriteColorFormat_256Color);
-	menu.gfx[1] = oamAllocateGfx(&oamSub, SpriteSize_16x16, SpriteColorFormat_256Color);
-	menu.gfx[2] = oamAllocateGfx(&oamSub, SpriteSize_16x16, SpriteColorFormat_256Color);
-	menu.gfx[3] = oamAllocateGfx(&oamSub, SpriteSize_16x16, SpriteColorFormat_256Color);
-	menu.gfx[4] = oamAllocateGfx(&oamSub, SpriteSize_16x16, SpriteColorFormat_256Color);
-
-	menu.gfx_frame = (u8*)spritesheetTiles;
+	for (u8 i = 0; i < 5; i++) {
+		menu.gfx[i] = oamAllocateGfx(&oamSub, SpriteSize_16x16, SpriteColorFormat_256Color);
+	}
+ 	menu.gfx_frame = (u8*)spritesheetTiles;
 
 	return menu;
 }
 
 void displayMenu(Menu * menu, bool hideMenu) {
-	for (int i = 0; i < 5; i++) {
-		u8* offset = menu->gfx_frame + (1 + i) * 16*16;
-		dmaCopy(offset, menu->gfx, 16*16);
+	for (u8 i = 0; i < 5; i++) {
+		u8* offset = menu->gfx_frame + (5 + i) * 16*16;
+		dmaCopy(offset, menu->gfx[i], 16*16);
 
 		oamSet(&oamSub,
 			2 + i, // oam entry id
