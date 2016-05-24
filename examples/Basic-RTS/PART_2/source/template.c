606a94b61c07142bb63d29cd42fefd395663de4a
John Riselvato <jdriselvato@gmail.com>
2016-05-24 18:23:15 -0400

    removed everything from app except touchPosition code and it still does not work. What am I missing here...

--- examples/Basic-RTS/PART_2/source/template.c
+++ examples/Basic-RTS/PART_2/source/template.c
@@ -13,11 +13,11 @@ Things to know:
 ---------------------------------------------------------------------------------*/
 #include <nds.h>
 #include <stdio.h>
-#include <spritesheet.h>
+// #include <spritesheet.h>
 
-#include "menu.h"
-#include "units.h"
-#include "house.h"
+// #include "menu.h"
+// #include "units.h"
+// #include "house.h"
 
 /*---------------------------------------------------------------------------------
 Global Variables
@@ -26,31 +26,32 @@ touchPosition touch; // Stylus location <-- why isn't this working...
 
 int main(int argc, char** argv) {
 	videoSetModeSub(MODE_0_2D); // Initialize the top screen engine
-	vramSetBankD(VRAM_D_SUB_SPRITE);
+	// vramSetBankD(VRAM_D_SUB_SPRITE);
 	consoleDemoInit();	// debugging
 
-	oamInit(&oamSub, SpriteMapping_1D_128, false);
-	dmaCopy(spritesheetPal, SPRITE_PALETTE_SUB, 512);
+	// oamInit(&oamSub, SpriteMapping_1D_128, false);
+	// dmaCopy(spritesheetPal, SPRITE_PALETTE_SUB, 512);
 
-	Menu menu = initMenu();
-	House house = initHouse();
+	// Menu menu = initMenu();
+	// House house = initHouse();
 	int touching = 0;
 	while(1) {
 		if(keysHeld() & KEY_TOUCH) {
 			touchRead(&touch);
-			touching = 1;
+			touching = 1; /// <-- it never gets here.. WHY!?!
 		}
-		stylusTouch(&menu, &touch);
-		if (menu.selectedIcon == 0) addToQueue(); // 0 = add unit
+		printf("\x1b[1;1HTouching: %d", touching);
 
-		generateHouse(&house);
-		bool selected = hideHouseMenu(&touch, &house);
-		displayMenu(&menu, selected);
+		// stylusTouch(&menu, &touch);
+		// if (menu.selectedIcon == 0) addToQueue(); // 0 = add unit
+
+		// generateHouse(&house);
+		// bool selected = hideHouseMenu(&touch, &house);
+		// displayMenu(&menu, selected);
 
 
 		// printf("\x1b[1;1HSelected Icon: %d | Menu: %d", menu.selectedIcon, selected);
 		// printf("\x1b[1;1Hmx %d, my %d | tx %d, ty %d", house.x, house.y, touch.px, touch.py);
-		printf("\x1b[1;1HTouching: %d", touching);
 		swiWaitForVBlank();
 		oamUpdate(&oamSub);
 	}
