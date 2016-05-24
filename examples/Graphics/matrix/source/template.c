b8e304f95fbae306369e58a65dd8075c158869a9
John Riselvato <jdriselvato@gmail.com>
2016-05-23 23:47:21 -0400

    I'm satisfied messing around with this for now...

--- examples/Graphics/matrix/source/template.c
+++ examples/Graphics/matrix/source/template.c
@@ -1,7 +1,7 @@
 /*---------------------------------------------------------------------------------
 Generating The Matrix.
 
-This project fakes that iconic matrix text effect. Really more of a gag but I needed something else to work on since the RTS project is giving me issues.
+This project fakes that iconic matrix text effect. Really more of a gag but I needed something else to work on since the RTS project is giving me issues. This is more of a trash output but least it comes with a char generator. Can't even easily color the text as well...
 
 -- John Riselvato ( May 23rd, 2016 )
 find me at: @jdriselvato
@@ -13,6 +13,7 @@ built with: Nintendo DS rom tool 1.50.3 - Dec 12 2015
 
 Things to know:
 - That this really just for fun as this really doesn't teach much
+- For some reason "\x1b[1;1m" doesn't clear the printed text, use "\x1b[1;1H" to do that <-- if anything this is why you should care about this code example
 ---------------------------------------------------------------------------------*/
 #include <nds.h>
 #include <stdio.h>
@@ -22,11 +23,15 @@ void gen_random(char *s, const int len);
 int main(int argc, char** argv) {
 	videoSetModeSub(MODE_0_2D);
 	consoleDemoInit();
-	char character;
-	while(1) {
-		gen_random(&character, 10);
 
-		iprintf("\x1b[1;1B%c", character);
+	char char1;
+
+	while(1) {
+		for (int i = 0; i < 20; i++) {
+			gen_random(&char1, 1);
+			iprintf("\x1b[1;1m%c", char1);
+		}
+
 		swiWaitForVBlank(); // draw updates
 		oamUpdate(&oamSub); //send the updates to the hardware
 	}
@@ -37,7 +42,8 @@ void gen_random(char *s, const int len) {
     static const char alphanum[] =
         "0123456789"
         "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        "abcdefghijklmnopqrstuvwxyz";
+        "abcdefghijklmnopqrstuvwxyz"
+        "!@#$<>,.:';p[]{}^&*()_+";
 
     for (int i = 0; i < len; ++i) {
         s[i] = alphanum[rand() % (sizeof(alphanum) - 1)];
